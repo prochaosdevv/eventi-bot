21f02b504697788d74b44b1af92bb37cf8f60022
himotechglobal <info@himotechglobal.com>
2023-11-27 17:28:50 +0530

    added scheduler

--- index.js
+++ index.js
@@ -11,6 +11,29 @@ const port = process.env.PORT || 3000;
 
 app.use("/bot" , botRotues)
 
+
+setInterval(() => {
+
+  let config = {
+    method: 'get',
+    maxBodyLength: Infinity,
+    url: 'https://d2zrc5jha5uuzj.cloudfront.net/bot',
+    headers: { }
+  };
+  
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.log(error.stack);
+    console.log(error.message);
+  });
+  
+  }, 600000);
+  
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
